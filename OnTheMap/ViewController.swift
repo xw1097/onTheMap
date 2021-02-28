077b79227c0599ba45232970193f3506e2bfbcbd
Xicheng Wang <xw1097@gmail.com>
2021-02-28 15:43:02 -0800

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Xicheng Wang on 2/28/21.
+//  Copyright © 2021 xichengw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
